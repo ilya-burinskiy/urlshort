f12fd447088175442b40aeb491236251c7dc7bd1
Ilya Burinsky <burinskiy992@gmail.com>
2023-11-07 15:48:12 +0300

    CreateShortenedURLService

---
+++ internal/app/services/create_shortened_url_service.go
@@ -0,0 +1,27 @@
+package services
+
+import "github.com/ilya-burinskiy/urlshort/internal/app/storage"
+
+type RandHexStringGenerator interface {
+	Call(n int) (string, error)
+}
+
+func CreateShortenedURLService(
+	originalURL,
+	shortenedURLBaseAddr string,
+	pathLen int,
+	randGen RandHexStringGenerator,
+) (string, error) {
+	shortenedURLPath, ok := storage.Get(originalURL)
+	if !ok {
+		var err error
+		shortenedURLPath, err = randGen.Call(pathLen)
+		if err != nil {
+			return "", err
+		}
+		storage.Put(originalURL, shortenedURLPath)
+	}
+
+	// TODO: maybe use some URL builder
+	return shortenedURLBaseAddr + "/" + shortenedURLPath, nil
+}
