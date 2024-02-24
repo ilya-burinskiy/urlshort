aeb6d235f1cee2393e5319bb8e70bbf81d329f46
Ilya Burinsky <burinskiy992@gmail.com>
2024-02-24 21:29:57 +0300

    models pkg doc

--- internal/app/models/record.go
+++ internal/app/models/record.go
@@ -1,5 +1,6 @@
 package models
 
+// Shortened URL model
 type Record struct {
 	OriginalURL   string `json:"original_url"`
 	ShortenedPath string `json:"shortened_path"`
--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -1,5 +1,6 @@
 package models
 
+// User model
 type User struct {
 	ID int `json:"id"`
 }
